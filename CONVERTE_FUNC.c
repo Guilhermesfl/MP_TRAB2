3393f9ddad0a1aecd3ad6357ada672e2cadff5d6
Guilhermesfl <gsfl3101@gmail.com>
2016-09-27 20:13:36 -0300

    Criação da Função de Verificação da validade dos algarismos romanos

--- CONVERTE_FUNC.c
+++ CONVERTE_FUNC.c
@@ -41,3 +41,35 @@ int CONVERTE_ROMANO(const char *num_romano){
 	}
 	return num_arabe;
 };
+
+/** \brief Função responsável por verificar a validade dos algarismos inseridos.*/
+int	 VERIFICA_ROMANO(const char *num_romano){
+
+	/**
+		\details Função utiliza o padrão presente nos números romanos para verificar a validade dos algarismos inseridos
+		\param num_romano contém os algarismos romanos.
+		\param i Variável que representa a flag de validade.
+		\return -1 indica que o a invalidez do número e 0 a validez
+
+	*/
+
+	int i=0;
+	while(num_romano[i]){
+		if(num_romano[i] != 'I' && num_romano[i] != 'X' && num_romano[i] != 'C'
+			&& num_romano[i] != 'L' && num_romano[i] != 'V' && num_romano[i] != 'M'
+			&& num_romano[i] != 'I') return -1;
+		else if(num_romano[i] == num_romano[i+1] && num_romano[i] == num_romano[i+2] && num_romano[i] == num_romano[i+3]
+			&& (num_romano[i+1]) && (num_romano[i+2]) && (num_romano[i+3])) return -1; 
+		else if (num_romano[i] == 'D' && num_romano[i+1] == 'M' && (num_romano[i+1])) return -1;
+		else if ((num_romano[i] == 'X' && num_romano[i+1] == 'D') || (num_romano[i] == 'X' && num_romano[i+1] == 'M')
+				&& (num_romano[i+1])) return -1;
+		else if ((num_romano[i] == 'I' && num_romano[i+1] != 'X') && (num_romano[i] == 'I' && num_romano[i+1] != 'V')
+		 		&& (num_romano[i] == 'I' && num_romano[i+1] != 'I') && (num_romano[i+1]))  return -1;
+		else if ((num_romano[i] == 'L' && num_romano[i+1] == 'D') || (num_romano[i] == 'L' && num_romano[i+1] == 'M')
+				||(num_romano[i] == 'L' && num_romano[i+1] == 'L') && (num_romano[i+1])) return -1;
+		else if ((num_romano[i] != 'X') && (num_romano[i] != num_romano[i+1]) && (num_romano[i] == num_romano[i+2])
+				&& (num_romano[i+1]) && (num_romano[i+2])) return -1;
+		i++;
+	}
+	return 0;
+}
